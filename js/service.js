28f7cf1b0912ac24c0dd7c5814dc629eb8201645
Mohamed Radwan <mohmedradwan944@gmail.com>
2018-09-16 21:18:01 +0200

    Add files via upload

---
+++ js/service.js
@@ -0,0 +1,69 @@
+/* Service page */
+function hover1() {
+	
+	var serviced =document.getElementById("service-4");
+	var servicec =document.getElementById("service-3");
+	var serviceb =document.getElementById("service-2");
+	var servicea =document.getElementById("service-1");
+	servicea.style.display="block";
+	serviceb.style.display="none";
+	servicec.style.display="none";
+	serviced.style.display="none";
+ }
+function hover2() {
+
+	var serviced =document.getElementById("service-4");
+	var servicec =document.getElementById("service-3");
+	var serviceb =document.getElementById("service-2");
+	var servicea =document.getElementById("service-1");
+	serviceb.style.display="block";
+	servicea.style.display="none";
+	servicec.style.display="none";
+	serviced.style.display="none";
+ } 
+ function hover3() {
+
+	var serviced =document.getElementById("service-4");
+	var servicec =document.getElementById("service-3");
+	var serviceb =document.getElementById("service-2");
+	var servicea =document.getElementById("service-1");
+	servicec.style.display="block";
+	serviceb.style.display="none";
+	servicea.style.display="none";
+	serviced.style.display="none";
+ }
+function hover4() {
+
+	var serviced =document.getElementById("service-4");
+	var servicec =document.getElementById("service-3");
+	var serviceb =document.getElementById("service-2");
+	var servicea =document.getElementById("service-1");
+	serviced.style.display="block";
+	serviceb.style.display="none";
+	servicec.style.display="none";
+	servicea.style.display="none";
+ }
+/* student area page */
+function click1() {
+    var services1=document.getElementById("home");
+    var services2=document.getElementById("chat");
+    var services3=document.getElementById("test");
+	services1.style.display="block"
+	services2.style.display="none"
+	services3.style.display="none" }
+
+function click2() {
+    var services1=document.getElementById("home");
+    var services2=document.getElementById("chat");
+    var services3=document.getElementById("test");
+	services2.style.display="block"
+	services1.style.display="none"
+	services3.style.display="none" }
+
+function click3() {
+    var services1=document.getElementById("home");
+    var services2=document.getElementById("chat");
+    var services3=document.getElementById("test");
+	services3.style.display="block"
+	services2.style.display="none"
+	services1.style.display="none" }
